319ea08e0a7a03132ee9d8d65d5986509c7e7e99
Brittany Howard <howardba@umich.edu>
2019-03-02 12:05:43 -0800

    made the snek a little smarter

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-import json
+=import json
 import os
 import random
 import bottle
 
 from api import ping_response, start_response, move_response, end_response
+
 
 @bottle.route('/')
 def index():
@@ -11,6 +12,7 @@ def index():
     Battlesnake documentation can be found at
        <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
     '''
+
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -22,6 +24,7 @@ def static(path):
     """
     return bottle.static_file(path, root='static/')
 
+
 @bottle.post('/ping')
 def ping():
     """
@@ -29,6 +32,7 @@ def ping():
     such as Heroku, from sleeping the application instance.
     """
     return ping_response()
+
 
 @bottle.post('/start')
 def start():
@@ -54,10 +58,47 @@ def move():
     TODO: Using the data from the endpoint request object, your
             snake AI must choose a direction to move in.
     """
-    print(json.dumps(data))
+    # dictionary with reward to choose each
+    directions = {'up': 1,
+                  'down': 1,
+                  'left': 1,
+                  'right': 1}
 
-    directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    board_height = data['board']['height']
+    board_width = data['board']['width']
+    head_x = data['you']['body']['x'][0]
+    head_y = data['you']['body']['y'][0]
+    neck_x = data['you']['body']['x'][1]
+    neck_y = data['you']['body']['y'][1]
+    other_sneks = data['board']['snakes']
+
+    for direction in ['up', 'down', 'left', 'right']:
+        reward = 0
+        # check if any of the moves will run you into a wall
+        if hit_wall(head_x, head_y, board_height, board_width, direction):
+            reward += -np.inf
+
+        # check which of the moves will run your head into your neck
+        if head_hit_neck(head_x, head_y, neck_x, neck_y, direction):
+            reward += -np.inf
+
+        # check if it's going to hit its own tail
+        if head_hit_tail(direction):
+            reward += -np.inf
+
+        # check if any of the moves will run you into another snake
+        if hit_other_snek(head_x, head_y, other_sneks, direction):
+            reward += -np.inf
+
+        # check which move will bring you closest to food
+        # assign rewards based on how much closer the food is
+        reward += assign_food_reward(head_x, head_y, data, direction)
+
+        # update directions dict with new reward
+        directions[direction] += reward
+
+    # choose direction with greatest reward
+    direction = keywithmaxval(directions)
 
     return move_response(direction)
 
@@ -84,3 +125,104 @@ if __name__ == '__main__':
         port=os.getenv('PORT', '8080'),
         debug=os.getenv('DEBUG', True)
     )
+
+
+# done
+def keywithmaxval(d):
+    """ a) create a list of the dict's keys and values;
+    b) return the key with the max value"""
+    v = list(d.values())
+    k = list(d.keys())
+    return k[v.index(max(v))]
+
+
+# done
+def hit_wall(head_x, head_y, board_height, board_width, direction):
+    # true if this move will cause you to hit a wall
+    dir_vect = direction_to_vector(direction)
+    if (head_x + dir_vect[0] == 0) or (head_x + dir_vect[0] == board_width):
+        return(True)
+    elif (head_y + dir_vect[1] == 0) or (head_x + dir_vect[1] == board_height):
+        return(True)
+    else:
+        return(False)
+
+
+# done
+def head_hit_neck(head_x, head_y, neck_x, neck_y, direction):
+    # true if this move will cause you to hit your head on your neck
+    dir_vect = direction_to_vector(direction)
+    new_head_x = head_x + dir_vect[0]
+    new_head_y = head_y + dir_vect[1]
+    if (new_head_x == neck_x) & (new_head_y == neck_y):
+        return(True)
+    else:
+        return(False)
+
+
+# done
+def hit_other_snek(head_x, head_y, other_sneks, direction):
+    # True if this move will cause you to collide with another snake
+    # according to their current position, not where they could move next
+    # also in future, take into account that their tail will shorten if they don't eat
+    dir_vect = direction_to_vector(direction)
+    new_head_coord = (head_x + dir_vect[0], head_y + dir_vect[1])
+
+    # get coordinates occupied by all other snakes
+    other_snek_body_coords = []
+    other_snek_possible_heads = []
+    for snek in other_sneks:
+        other_snek_body_coords.append((snek['body']['x'], snek['body']['y']))
+        # also avoid all possible pixels the other snek could go to
+        other_snek_possible_heads.append(get_surrounding_coords(snek['body']['x'][0], snek['body']['y'][0]))
+        # for later if time, check if the other snake can access food,
+        # if not then take tail coord out of avoid list
+    avoid_coords = other_snek_body_coords + other_snek_possible_heads
+    if new_head_coord in avoid_coords:
+        return True
+    else:
+        return False
+
+
+# done
+def assign_food_reward(head_x, head_y, data, direction):
+    new_head_coord = (head_x + direction_to_vector(direction)[0], head_y + direction_to_vector(direction)[1])
+    # make an array of board shape
+    # 0s where no food
+    # 1s where yes food
+    # food_array = np.zeros(data['board']['height'], data['board']['height'])
+    food_reward = 0
+    for food_spot in data['board']['food']:
+        # food_array[food_spot['x'], food_spot['y']] = 1
+        # if we turn this direction, how many more turns
+        # would it take use to get to this food?
+        n_turns_to_food = abs(new_head_coord[0] - food_spot['x']) + abs(new_head_coord[1] - food_spot['y'])
+        # assign reward based on maximum possible number of turns
+        max_turns = data['board']['height'] + data['board']['height']
+        food_reward += max_turns - n_turns_to_food
+    return(food_reward)
+
+
+########################################
+# utils
+########################################
+
+
+# done
+def direction_to_vector(direction):
+    # convert a direction to a vector
+    foo = {'up': (1, 0),
+           'down': (-1, 0),
+           'left': (0, -1),
+           'right': (0, 1)
+           }
+    return(foo[direction])
+
+
+def get_surrounding_coords(x_coord, y_coord):
+    # given an (x,y) coordinate, get all surrounding
+    up_new = (x_coord, y_coord+1)
+    down_new = (x_coord, y_coord-1)
+    left_new = (x_coord-1, y_coord)
+    right_new = (x_coord+1, y_coord)
+    return([up_new, down_new, left_new, right_new])
