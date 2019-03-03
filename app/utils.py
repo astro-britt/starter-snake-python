3c12c7b77d2ddbddbe1ba9f9ab2d35c4c6da289d
Brittany Howard <howardba@umich.edu>
2019-03-02 16:01:05 -0800

    nevermind

--- app/utils.py
+++ app/utils.py
@@ -3,8 +3,8 @@ import numpy as np
 # done
 def direction_to_vector(direction):
     # convert a direction to a vector
-    foo = {'up': (0, 1),
-           'down': (0, -1),
+    foo = {'up': (0, -1),
+           'down': (0, 1),
            'left': (-1, 0),
            'right': (1, 0)
            }
